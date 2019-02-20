31e9deb807e1f2ed7d1fc5bd562bbbaf41d32f07
Nagy Tamás Zsolt <47737070+Xeno1080@users.noreply.github.com>
2019-02-20 08:21:43 +0100

    Add files via upload

---
+++ ts/content.ts
@@ -0,0 +1,34 @@
+﻿import * as http from "http";
+
+export default class Content {
+
+    public content(req: http.IncomingMessage, res: http.ServerResponse): void {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+        res.write("<h1 style='color: red;'>Elágazások</h1>");
+
+        var i = true;
+        if (i) {
+            res.write("<p>if true</p>");
+            i = false;
+        } else {
+            res.write("<p>else true</p>");
+        }
+        if (i) {
+            res.write("<p>if true</p>");
+        } else {
+            res.write("<p>else true</p>");
+        }
+
+        switch ("A") {
+            case "A":
+                res.write("<p>switch-case elágazás </p>");
+                break;
+            default:
+                break;
+        }
+
+        res.write("<a href='https://github.com/Xeno1080/Elagazasok' target='_blank'>" +
+            "https://github.com/Xeno1080/Elagazasok</a><br>");
+        res.end();
+    }
+}
